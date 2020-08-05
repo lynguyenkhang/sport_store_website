fe174573bb59d9ab33599569d0abe39465ba2d56
Ly Nguyen Khang <nguyenkhang@Lys-MacBook-Pro.local>
2020-08-05 16:50:20 +0700

    Make ajax for 'Mua hang' button, and fix mistakes in 'gio hang' page

--- publics/jquery/productsInCart.js
+++ publics/jquery/productsInCart.js
@@ -20,6 +20,61 @@ $(document).ready(function(){
 // 	console.log(cart[id]);
 // }
 
+function buyProduct(sessionId, productId, product_link){
+	var productsListInCart = document.getElementById("productsListInCart");
+
+	var product_title = document.querySelector(".details-title").innerText;;
+	var quantity = document.getElementById('quantity-box').value;
+	quantity = JSON.parse(quantity);
+	var img_link = document.querySelector(".details-products-img > img").src;
+	var priceStr = document.querySelector(".details-price > .price").innerText;
+	// badge-cart
+	var badge_cart = document.querySelector(".badge-cart");
+
+	// total Price
+	var totalPriceElement = document.querySelectorAll(".total-price > span")[1];
+	var totalPrice = totalPriceElement.innerText;
+	var price = removeWordAndParseInt(priceStr, '.');
+	totalPrice = removeWordAndParseInt(totalPrice, '.');
+	price = price * quantity;
+	var newPrice = JSON.stringify(totalPrice + price);
+	newPrice = addVND(newPrice);
+
+	// Giỏ hàng title (tổng só sản phẩm)
+	var gio_hang = document.querySelector(".gio-hang-content");
+	var gio_hang_number = gio_hang.querySelector("span");
+	var begin = gio_hang_number.innerText.indexOf("(") + 1;
+	var end = gio_hang_number.innerText.indexOf(")");
+	var currentNumber = JSON.parse(gio_hang_number.innerText.slice(begin, end)) + quantity;
+
+	//cart
+	var cart = crawlProductsInCart();
+
+	if(cart[`${productId}`]){
+		// buy-number-box
+		var element = document.getElementById(productId);
+		var input = element.querySelector("input");
+		cart[`${productId}`] += quantity;
+		input.value = cart[`${productId}`];
+	} else {
+		cart[`${productId}`] = quantity;
+		var content = `<li class="id-product-box" id="${productId}"><div class="list-cart-img-box"><img src="${img_link}"></div><div class="list-cart-content"><a class="list-cart-content-title" href="${product_link}">${product_title}</a><div class="list-cart-content-price"><span class="text-muted" style="font-size: 13.5px;">Số lượng</span><span class="product-in-cart-price">${priceStr}</span></div><div class="list-cart-content-quantity"><button onclick="increaseQuantity('${sessionId}','${productId}', '${priceStr}')">+</button><input class="buy-number-box buy-number-box-2" value="${quantity}"><button onclick="decreaseQuantity('${sessionId}','${productId}', '${priceStr}')">-</button></div><a class="list-cart-content-unchoose-product text-primary" href="#no" onclick="deleteProduct('${sessionId}','${productId}', '${priceStr}')">Bỏ sản phẩm</a></div></li>`;
+		productsListInCart.innerHTML += content;
+	}
+
+	alert("Khách hàng mua hàng thành công.");
+	axios({
+		method: 'patch',
+		url: `/api/productsInCart/${sessionId}`,
+		data: cart
+	}).then(function(res){
+		badge_cart.innerText = `${currentNumber}`;
+		gio_hang_number.innerText = `(${currentNumber}) `;
+		totalPriceElement.innerText = newPrice;
+	})
+
+	console.log('test');
+}
 
 
 function increaseQuantity(sessionId, productId, priceStr){
@@ -32,6 +87,8 @@ function increaseQuantity(sessionId, productId, priceStr){
 	var element_gh =  document.getElementById(`${productId}_gh`);
 	if(element_gh){
 		var input_gh = element_gh.querySelector("input");
+		var gio_hang_number_gh = document.querySelectorAll(".gio-hang-page-product-title > span")[1];
+		var totalPriceElement_gh = document.querySelector(".gio-hang-page-button-totalPrice");
 	};
 
 	// badge-cart
@@ -40,7 +97,10 @@ function increaseQuantity(sessionId, productId, priceStr){
 	// Giỏ hàng title (tổng só sản phẩm)
 	var gio_hang = document.querySelector(".gio-hang-content");
 	var gio_hang_number = gio_hang.querySelector("span");
-	var currentNumber = JSON.parse(gio_hang_number.innerText[1]) + 1;
+	var begin = gio_hang_number.innerText.indexOf("(") + 1;
+	var end = gio_hang_number.innerText.indexOf(")");
+	var currentNumber = JSON.parse(gio_hang_number.innerText.slice(begin, end)) + 1;
+
 
 	// crawl "cart" objective and increase value.
 	var cart = crawlProductsInCart();
@@ -64,7 +124,12 @@ function increaseQuantity(sessionId, productId, priceStr){
 		badge_cart.innerText = `${currentNumber}`;
 		gio_hang_number.innerText = `(${currentNumber}) `;
 		input.value = quantity;
-		if(input_gh){ input_gh.value = quantity;};
+		if(input_gh){
+			input_gh.value = quantity;
+			gio_hang_number_gh.innerText = `(${currentNumber}`;
+			totalPriceElement_gh.innerText = newPrice;
+		};
+
 		totalPriceElement.innerText = newPrice;		
 	}).catch(function(err){
 		console.log(err);
@@ -83,6 +148,8 @@ function decreaseQuantity(sessionId, productId, priceStr){
 	var element_gh =  document.getElementById(`${productId}_gh`);
 	if(element_gh){
 		var input_gh = element_gh.querySelector("input");
+		var gio_hang_number_gh = document.querySelectorAll(".gio-hang-page-product-title > span")[1];
+		var totalPriceElement_gh = document.querySelector(".gio-hang-page-button-totalPrice");
 	};
 
 	// badge-cart
@@ -91,7 +158,9 @@ function decreaseQuantity(sessionId, productId, priceStr){
 	// Giỏ hàng title (tổng só sản phẩm)
 	var gio_hang = document.querySelector(".gio-hang-content");
 	var gio_hang_number = gio_hang.querySelector("span");
-	var currentNumber = JSON.parse(gio_hang_number.innerText[1]) - 1;
+	var begin = gio_hang_number.innerText.indexOf("(") + 1;
+	var end = gio_hang_number.innerText.indexOf(")");
+	var currentNumber = JSON.parse(gio_hang_number.innerText.slice(begin, end)) - 1;
 
 	if(!quantity){
 		quantity = 1;
@@ -117,8 +186,14 @@ function decreaseQuantity(sessionId, productId, priceStr){
 			// change value in html
 			badge_cart.innerText = `${currentNumber}`;
 			gio_hang_number.innerText = `(${currentNumber}) `;
+
 			input.value = quantity;
-			if(input_gh){ input_gh.value = quantity;};
+			if(input_gh){
+				input_gh.value = quantity;
+				gio_hang_number_gh.innerText = `(${currentNumber}`;
+				totalPriceElement_gh.innerText = newPrice;
+			};
+
 			totalPriceElement.innerText = newPrice;
 		}).catch(function(err){
 			console.log(err);
@@ -130,7 +205,13 @@ function decreaseQuantity(sessionId, productId, priceStr){
 
 function deleteProduct(sessionId, productId, priceStr){
 	var element = document.getElementById(productId);
-	var element_gh = document.getElementById(`${productId}_gh`);
+	var element_gh =  document.getElementById(`${productId}_gh`);
+	if(element_gh){
+		var input_gh = element_gh.querySelector("input");
+		var gio_hang_number_gh = document.querySelectorAll(".gio-hang-page-product-title > span")[1];
+		var totalPriceElement_gh = document.querySelector(".gio-hang-page-button-totalPrice");
+	};
+
 	var badge_cart = document.querySelector(".badge-cart");
 
 	//crawl "cart" objective
@@ -152,9 +233,6 @@ function deleteProduct(sessionId, productId, priceStr){
 	var gio_hang_number = gio_hang.querySelector("span");
 	var begin = gio_hang_number.innerText.indexOf("(") + 1;
 	var end = gio_hang_number.innerText.indexOf(")");
-
-
-
 	var currentNumber = JSON.parse(gio_hang_number.innerText.slice(begin, end)) - quantity;
 
 
@@ -165,10 +243,15 @@ function deleteProduct(sessionId, productId, priceStr){
 	}).then(function(res){
 		// change value in html
 		element.setAttribute("style", "display: none;");
-		if(element_gh) { element_gh.setAttribute("style", "display: none;"); };
+		if(element_gh) {
+			element_gh.setAttribute("style", "display: none;"); 
+			gio_hang_number_gh.innerText = `(${currentNumber}`;
+			totalPriceElement_gh.innerText = newPrice;
+		};
 		badge_cart.innerText = `${currentNumber}`;
 		gio_hang_number.innerText = `(${currentNumber}) `;
 		totalPriceElement.innerText = newPrice;
+
 	}).catch(function(err){
 		console.log(err);
 	})
