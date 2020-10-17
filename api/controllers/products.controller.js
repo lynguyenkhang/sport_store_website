c9ec130cecc3159a98449c4bf8d50ac08e83303a
Ly Nguyen Khang <nguyenkhang@Lys-MacBook-Pro.local>
2020-10-17 23:42:54 +0700

    Fixed remove conflict changes

--- api/controllers/products.controller.js
+++ api/controllers/products.controller.js
@@ -1,10 +1,6 @@
 const Products = require('../../models/products.model.js');
 
 module.exports.getAll = async function(req, res){
-<<<<<<< HEAD
-	const products = await Products.find();
-=======
 	let products = await Products.find();
->>>>>>> image_cloudinary
 	res.json(products);
 }
--- index.js
+++ index.js
@@ -1,35 +1,5 @@
 require('dotenv').config();
 
-<<<<<<< HEAD
-// declare package
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-
-// declare middlewares
-const sessionMiddleware = require('./middlewares/session.middleware.js');
-const brandsMiddleware = require('./middlewares/brandsList.middleware.js');
-const productMiddleware = require('./middlewares/products.middleware.js');
-
-// declare routes
-const productsRoute = require('./routes/products.route.js');
-const cartRoute = require('./routes/cart.route.js');
-const apiProductsInCartRoute = require('./api/routes/productsInCart.route.js');
-const apiProductsRoute = require('./api/routes/products.route.js');
-
-// set configure
-mongoose.connect(process.env.MONGOLAB_ROSE_URI);	
-app.set('view engine', 'pug');
-app.set('views', './views');
-
-// use middlewares
-app.use(express.static('publics'));
-app.use(express.json());
-app.use(cookieParser(process.env.secretCookie));
-=======
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -61,7 +31,6 @@ const cartRoute = require('./routes/cart.route.js');
 const apiProductsInCartRoute = require('./api/routes/productsInCart.route.js');
 const apiProductsRoute = require('./api/routes/products.route.js');
 
->>>>>>> image_cloudinary
 app.use(sessionMiddleware.sessionId);
 app.use(brandsMiddleware.list);
 
@@ -70,12 +39,8 @@ app.use('/api/productsInCart', apiProductsInCartRoute);
 app.use('/api/products', apiProductsRoute);
 
 app.get('/', productMiddleware.load,function(req,res,next){
-<<<<<<< HEAD
-	// console.log(req.products[0]);
-	// constiable "rackets", "shoeslist",... tự đc đưa vào file pug
-=======
+
 	// variable "rackets", "shoeslist",... tự đc đưa vào file pug
->>>>>>> image_cloudinary
 	// k cần ghi thêm vào phần render
 
 	res.render('index',{
